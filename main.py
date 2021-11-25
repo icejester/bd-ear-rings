a9fede70012165181c1bb2ec335217c58458767c
Jared Mallas <jrmallas@icloud.com>
2021-11-24 19:25:02 -0600

    v1.0.0

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import neopixel
 import random
 
 # NeoPixel strip (of 16 LEDs) connected on D4
-NUMPIXELS = 16
+NUMPIXELS = 12
 
 neopixels = neopixel.NeoPixel(D3, NUMPIXELS, brightness=1, auto_write=True, pixel_order=neopixel.GRBW)
 DIRECTION = 1 # 1 == "up"
-COLOR = 1 # 1 == "red"
+COLOR = 1 # 1 == "flicker" # 2 == "rainbow"
 
 ######################### HELPERS ##############################
 
@@ -41,11 +41,6 @@ def wheel(pos):
 def flicker(idx, rgbVal):
     neopixels.brightness = 1
     neopixels[idx] = rgbVal
-    if idx > 2 and idx < NUMPIXELS -2:
-        neopixels[idx -2] = rgbVal
-        neopixels[idx -1] = rgbVal
-    time.sleep(0.0125)
-    neopixels.fill = (0, 0, 0)
     neopixels.brightness = .025
 
 def rainbowPulse(i):
@@ -79,64 +74,34 @@ def whitePulse():
     if DIRECTION == 2:
         neopixels.fill((rCur - 15, gCur - 10, bCur - 10))
 
+def shiftColors():
+    print("In colorChange...")
+    print(COLOR)
+    if COLOR == 1:
+        COLOR = 2
+    elif COLOR == 2:
+        COLOR = 1
+    print(COLOR)
+
 ######################### MAIN LOOP ##############################
 
 i = 0;
-colorChange = 0;
-
+colorScheme = 1;
 while True:
-    # if touch.value:
-    #     neopixels.brightness = 1
-    #     neopixels.fill((0, 0, 0))
-    #     flicker(random.randint(0, (NUMPIXELS-1)),(255, 255, 255))
-    #     colorChange = 1;
-    #   # print("D3 touched!")
-    # else:
-    if colorChange:
-        # print("Changing color!")
-        if COLOR == 1:
-            COLOR = 2
-            # print("color change to 2")
-            # print(COLOR)
-        elif COLOR == 2:
-            COLOR = 3
-        elif COLOR == 3:
-            COLOR = 4
-        elif COLOR == 4:
-            COLOR = 1
-        else:
-            COLOR = 1
-            # print("color change to 1")
-        colorChange = 0
-        if random.randint(0, 10) == 5:
-            COLOR = 3
-
-    aPixel = neopixels[0]
-    rCur = aPixel[0]
-    # print(rCur)
-    if rCur >= 244:
-        DIRECTION = 2
-    elif rCur <= 15:
-        DIRECTION = 1
-    if COLOR == 1:
-        redPulse()
-    elif COLOR == 2:
-        whitePulse()
-    elif COLOR == 3:
+    if colorScheme == 1:
+        for x in range(random.randint(15, 25)):
+            flicker(random.randint(0, (NUMPIXELS-1)),(199,21,133))
+            neopixels.fill((0,0,0))
+            time.sleep(0.0125)
+        colorScheme = 2
+    elif colorScheme == 2:
         rainbowPulse(i)
-    elif COLOR == 4:
-        flicker(random.randint(0, 12),(255, 255, 255))
+        time.sleep(0.0125)
 
     i = (i+30) % 256  # run from 0 to 255
-    neopixels.brightness = .025
-    time.sleep(.05) # make bigger to slow down
 
-    if random.randint(0, 1000) <= 50:
-        neopixels.fill((0, 0, 0))
-        neopixels.brightness = 1
-        flicker(random.randint(0, (NUMPIXELS)),(255, 255, 255))
-        colorChange = 1
-    ## rainbowPulse(i)
-    ## edPulse()
-    # neopixels[0] = (0, 0, 0)
-    # time.sleep(0.1)
+    if random.randint(0, 100) == 71:
+        if colorScheme == 1:
+            colorScheme=2
+        elif colorScheme == 2:
+            colorScheme=1
